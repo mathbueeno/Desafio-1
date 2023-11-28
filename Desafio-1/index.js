3ac441fb45c40bbf2c2805e708a6d0082cdbfd08
mathbueeno <mathbueeno@gmail.com>
2023-11-28 19:04:21 -0300

    Desafio realizado

---
+++ Desafio-1/index.js
@@ -0,0 +1,26 @@
+// Criação da Váriavel 
+function calculoDivisiveis() {
+
+    // Criação das váriaveis 
+    const numero = parseInt(prompt("Insira o número desejado"));
+    let soma = 0;
+
+    // Verificação dos inteiros
+    if (isNaN(numero) || numero <= 0) {
+        console.log("Insira um número válido. Tente novamente");
+        return
+    }
+
+    // Cálculo dos divisíveis
+    for (let i = 1; i < numero; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            soma += i;
+        }
+    }
+
+    // Resposta para o user
+    console.log(`A soma dos valores divisíveis por 3 e 5 até ${numero} é: ${soma}`)
+
+}
+
+calculoDivisiveis();
